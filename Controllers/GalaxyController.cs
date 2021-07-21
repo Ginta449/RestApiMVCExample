89c49676bfb2177873d2e511f74a452565bada78
Ginta Kivite <86599036+Ginta449@users.noreply.github.com>
2021-07-21 15:42:29 +0100

    Add files via upload

---
+++ Controllers/GalaxyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCRestApi.Models;
+using Newtonsoft.Json;
+
+namespace MVCRestApi.Controllers
+{
+    public class GalaxyController : Controller
+    {
+        string server = "https://localhost:44369/";
+
+        public async Task<IActionResult> Index()
+        {
+            List<Galaxies> galaxyInfo = new List<Galaxies>();
+
+            using (var client = new HttpClient())
+            {
+               
+                client.BaseAddress = new Uri(server);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = await client.GetAsync("GalaxyApi/GalaxyApi.json");
+             
+                if (Res.IsSuccessStatusCode)
+                {
+                  
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                   
+                    galaxyInfo = JsonConvert.DeserializeObject<List<Galaxies>>(EmpResponse);
+                }
+            
+                return View(galaxyInfo);
+            }
+        }
+    }
+}
---
+++ Models/Galaxies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRestApi.Models
+{
+    public class Galaxies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public string ImageNameTwo { get; set; }
+
+    }
+}
